aa2ad839e1359db66163c4e5c1b8dc8ac2374f1d
victorliaocs <34571685+victorliaocs@users.noreply.github.com>
2017-12-20 21:28:51 +0800

    Add files via upload

---
+++ yinyang.py
@@ -0,0 +1,9 @@
+# Demo file for Spyder Tutorial
+# Hans Fangohr, University of Southampton, UK
+
+def hello():
+    """Print "Hello--- World9" and return None"""
+    print("Hello to the World")
+
+# main program starts here
+hello()
