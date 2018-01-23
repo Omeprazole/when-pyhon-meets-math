4a117293fba5ce417708bbaf0d1360bd05b33229
victorliaocs <34571685+victorliaocs@users.noreply.github.com>
2018-01-23 11:13:44 +0800

    delete

--- yinyang.py
+++
@@ -1,9 +0,0 @@
-# Demo file for Spyder Tutorial
-# Hans Fangohr, University of Southampton, UK
-
-def hello():
-    """Print "Hello--- World9" and return None"""
-    print("Hello to the World")
-
-# main program starts here
-hello()
